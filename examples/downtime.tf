6da2a648c1d3ed6a98a64088d945495ceddd61a2
Julien Riou <julien@riou.xyz>
2026-03-16 23:14:12 +0100

    docs(icinga2_downtime): Add example
    Signed-off-by: Julien Riou <julien@riou.xyz>

---
+++ examples/downtime.tf
@@ -0,0 +1,21 @@
+resource "icinga2_downtime" "test" {
+  type         = "Host"
+  filter       = "host.name==\"test\""
+  author       = "terraform"
+  comment      = "Initial downtime"
+  start_time   = time_static.downtime_start.unix
+  end_time     = time_static.downtime_start.unix + 3600
+  all_services = false
+  depends_on = [
+    icinga2_host.test
+  ]
+}
+
+resource "time_static" "downtime_start" {}
+
+resource "icinga2_host" "test" {
+  hostname      = "test"
+  address       = "127.0.0.1"
+  check_command = "hostalive"
+  zone          = "master"
+}
